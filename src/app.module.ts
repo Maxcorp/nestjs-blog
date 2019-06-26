6361c649ff40fbaa42860dbd26478c3017ef8aa0
maxcorp <shaparyuk@gmail.com>
2019-06-26 23:23:31 +0300

    generated categories

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,14 @@ import { PostsModule } from './posts/posts.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), PostsModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    PostsModule,
+    AuthModule,
+    CategoriesModule,
+  ],
 })
 export class AppModule {}
