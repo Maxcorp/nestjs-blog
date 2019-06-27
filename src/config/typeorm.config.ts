15199472376689557a6e70309c6722f8e30bbd22
maxcorp <shaparyuk@gmail.com>
2019-06-27 23:21:57 +0300

    added some .env vars to ormconfig

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,9 +1,9 @@
 import {TypeOrmModuleOptions} from '@nestjs/typeorm';
 
 const TYPEORM_HOST = process.env.DB_HOST;
-const TYPEORM_USERNAME = 'postgres';
-const TYPEORM_PASSWORD = 'changeme';
-const TYPEORM_DATABASE = 'blog';
+const TYPEORM_USERNAME = process.env.DB_USERNAME;
+const TYPEORM_PASSWORD = process.env.DB_PASSWORD;
+const TYPEORM_DATABASE = process.env.DB_NAME;
 const TYPEORM_PORT = 5432;
 const TYPEORM_SYNCHRONIZE = process.env.DB_SYNCHRONIZE;
 
